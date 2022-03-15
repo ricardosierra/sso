4d0b472a54f97983e1d4ef767b481f1aea0285f8
Ricardo R. Sierra <sierra.csi@gmail.com>
2022-03-15 10:52:42 -0300

    - Exceptions - ELoquent de Brokers - Commandos para gerar Brokers

---
+++ app/Models/Broker.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+// use Spatie\Permission\Traits\HasPermissions;
+// use Spatie\Permission\Traits\HasRoles;
+// use Spatie\Sluggable\HasSlug;
+// use Spatie\Sluggable\SlugOptions;
+
+class Broker extends Model
+{
+
+    // use HasRoles, HasSlug;
+
+    /**
+     * Get the table associated with the model.
+     *
+     * @return string
+     */
+    public function getTable()
+    {
+        return config('sso.brokersTable', 'brokers');
+    }
+
+    // /**
+    //  * Get the options for generating the slug.
+    //  */
+    // public function getSlugOptions() : SlugOptions
+    // {
+    //     return SlugOptions::create()
+    //         ->generateSlugsFrom('name')
+    //         ->saveSlugsTo('name-slug');
+    // }
+}
--- config/sso.php
+++ config/sso.php
@@ -7,4 +7,52 @@ return [
     'server_prefix' => env('SSO_SERVER_PREFIX', '/'),
     'type' => env('SSO_TYPE', 'broker'),
     'log_erros' => env('APP_DEBUG', false),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Server Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    //Do you want to serve the login form on your server?
+    'login_on_server' => true,
+
+    //In case of no redirect defined, where we'll redirect him?
+    'no_redirect_defined' => [
+        'mode' => 'url',
+        'destination' => '/home',
+    ],
+
+    //When Serving the login form on your server, what's the login form view?
+    'login_view' => 'login',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Brokers Configuration
+     |--------------------------------------------------------------------------
+     */
+
+    // Table used in App\Models\Broker model
+    'brokersTable' => 'brokers',
+    'brokersModel' => App\Models\Broker::class,
+
+    /*
+     |--------------------------------------------------------------------------
+     | User Configuration
+     |--------------------------------------------------------------------------
+     */
+
+    'usersModel' => \App\User::class,
+
+    'userTableName' => 'users',
+    'userUserNameField' => 'email',
+    'userPasswordField' => 'password',
+
+    // Logged in user fields sent to brokers.
+    'userFields' => [
+        // Return array field name => database column name
+        'id' => 'id',
+    ],
+
 ];
